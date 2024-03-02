0a7cc633adbca3ba01ce94c82b7996bd5e9c682c
Meena-Sakthi <meenasruthipranav@gmail.com>
2024-03-02 12:32:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import BookList from "./Components/Tasks/BookList";
-import ProfileTabs from "./Components/Tasks/Tabs/ProfileTabs";
+import BookList from "./Components/Tasks/BookList";
+// import ProfileTabs from "./Components/Tasks/Tabs/ProfileTabs";
 // import Tabs from "./Components/Tasks/Tabs/Tabs";
 function App() {
   return (
     <div>
       {/* <Tabs /> */}
-      <ProfileTabs />
-      {/* <BookList /> */}
+      {/* <ProfileTabs /> */}
+      <BookList />
     </div>
   );
 }
